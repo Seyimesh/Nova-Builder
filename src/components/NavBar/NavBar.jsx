c6c61f7092b4b43658c76dce2b7369b22df73f81
SeyiMesh <seyimeshileya@gmail.com>
2024-04-17 23:47:04 +0100

    Footer page fixed

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { RiMenu4Line, RiCloseFill } from "react-icons/ri";
 import MobileNavBar from "./MobileNavBar";
+import { Link } from "react-scroll";
 
 const dot = (
   <div className="bg-darkBlue mx-2 self-center rounded-full h-[6px] w-[6px] "></div>
@@ -14,7 +15,7 @@ const NavBar = () => {
   };
   return (
     <>
-      {isMenu && <MobileNavBar setIsMenu={setIsMenu} />}
+      {isMenu && <MobileNavBar isOpen={isMenu} setIsMenu={setIsMenu} />}
       <div className="bg-background sticky top-0 z-10">
         <nav className="max-w-screen-xl mx-auto py-4 px-6">
           <div className="flex items-center justify-between">
@@ -22,22 +23,48 @@ const NavBar = () => {
               <span className="font-bold text-2xl text-darkBlue">N</span>
               <div className="bg-darkBlue mx-0 self-center rounded-full h-[6px] w-[6px] "></div>
               <span className="font-bold text-2xl text-darkBlue">B</span>
-            </div>{" "}
+            </div>
             <ul className="hidden md:flex md:gap-12">
               <li>
-                <a className="menu-item">Home</a>
+                <Link to="hero" smooth spy offset={-80} className="menu-item">
+                  Home
+                </Link>
               </li>
               <li>
-                <a className="menu-item">Services</a>
+                <Link
+                  to="services"
+                  smooth
+                  spy
+                  offset={-80}
+                  className="menu-item"
+                >
+                  Services
+                </Link>
+              </li>
+              {/* <li>
+                <Link to="hero" smooth spy offset={-80} className="menu-item">Services</Link>
+              </li> */}
+              <li>
+                <Link
+                  to="our-work"
+                  smooth
+                  spy
+                  offset={-80}
+                  className="menu-item"
+                >
+                  Our Work
+                </Link>
               </li>
               <li>
-                <a className="menu-item">Services</a>
-              </li>
-              <li>
-                <a className="menu-item">Our Work</a>
-              </li>
-              <li>
-                <a className="menu-item">Testimonial</a>
+                <Link
+                  to="testimonial"
+                  smooth
+                  spy
+                  offset={-80}
+                  className="menu-item"
+                >
+                  Testimonial
+                </Link>
               </li>
             </ul>
             <button className="hidden h-10 bg-primary text-white text-sm px-6 rounded hover:bg-blue-200 hover:text-primary md:block">
