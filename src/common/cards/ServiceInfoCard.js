bdcaa1586da8d9085ca3644e5e63927b6ea9c03f
SeyiMesh <seyimeshileya@gmail.com>
2024-06-23 17:52:23 +0100

    Website updated

--- src/common/cards/ServiceInfoCard.js
+++ src/common/cards/ServiceInfoCard.js
@@ -1,18 +1,19 @@
 import React from "react";
 
-
 const ServiceInfoCard = ({ imgUrl, title, description }) => {
   return (
     <div className="flex flex-col md:even:flex-row-reverse md:odd:flex-row bg-secondary">
-      <div className="flex-1"> 
+      <div className="flex-1">
         <img src={imgUrl} alt={title} className="w-full h-80 md:h-full" />
       </div>
       <div className="flex-1">
         <div className="p-10">
-          <h6 className="text-md md:text-xl text-darkBlue font-medium mb-3">
+          <h6 className="lg:text-[26px] md:text-xl text-darkBlue font-bold mb-4">
             {title}
           </h6>
-          <p className="text-sm md:text-sm leading-6 whitespace-pre-line text-darkBlue">{description}</p>
+          <p className="text-sm md:text-sm lg:leading-6 lg:tracking-[0.03rem] text-darkBlue">
+            {description}
+          </p>
         </div>
       </div>
     </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,46 +3,38 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <div className="max-w-[1200px] max-auto bg-dimBlue">
-        <div className="flex flex-col items-center justify-center p-8">
-          <div className="flex items-center">
-            <span className="font-bold text-2xl text-darkBlue">N</span>
-            <div className="bg-darkBlue mx-0 self-center rounded-full h-[6px] w-[6px] "></div>
-            <span className="font-bold text-2xl text-darkBlue">B</span>
-          </div>
-
-          <ul className="flex justify-center gap-6 flex-wrap">
-            <li>
-              <a href="" className="text-sm text-darkBlue hover:font-medium">
-                About Us
-              </a>
-            </li>
-            <li>
-              <a href="" className="text-sm text-darkBlue hover:font-medium">
-                Contact Us
-              </a>
-            </li>
-            <li>
-              <a href="" className="text-sm text-darkBlue hover:font-medium">
-                Services
-              </a>
-            </li>
-            <li>
-              <a href="" className="text-sm text-darkBlue hover:font-medium">
-                Terms & Condictions
-              </a>
-            </li>
-            <li>
-              <a href="" className="text-sm text-darkBlue hover:font-medium">
-                Get Quotes
-              </a>
-            </li>
-          </ul>
-        </div>
+      <div className="max-w-[1200px] mx-auto bg-dimBlue p-8 flex justify-center">
+        <ul className="flex justify-center text-center gap-6 flex-wrap">
+          <li>
+            <a href="" className="text-sm text-darkBlue hover:font-medium">
+              About Us
+            </a>
+          </li>
+          <li>
+            <a href="" className="text-sm text-darkBlue hover:font-medium">
+              Contact Us
+            </a>
+          </li>
+          <li>
+            <a href="" className="text-sm text-darkBlue hover:font-medium">
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="" className="text-sm text-darkBlue hover:font-medium">
+              Terms & Conditions
+            </a>
+          </li>
+          <li>
+            <a href="" className="text-sm text-darkBlue hover:font-medium">
+              Get Quotes
+            </a>
+          </li>
+        </ul>
       </div>
 
       <div className="bg-darkBlue text-sm text-white text-center p-4">
-      © 2024. All Right Reserved
+        © 2024. All Right Reserved
       </div>
     </>
   );
